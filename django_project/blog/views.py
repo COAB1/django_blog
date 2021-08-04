77ec1c6f3270d19cab66730d13b30889c668fb6a
COAB1 <carol.o.ag@gmail.com>
2021-08-04 18:30:31 -0300

    Remove dummy data hard coded

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'COAB1',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 2, 2021'
-    },
-    {
-        'author': 'Jne Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 3, 2021'
-    }
-]
 
 def home (request):
     context = {
