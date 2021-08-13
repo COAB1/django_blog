4c3cc0020d1197aaa2bbc928d08e82e705038186
COAB1 <carol.o.ag@gmail.com>
2021-08-13 19:38:51 -0300

    Fix bug when resizing image

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -9,10 +9,11 @@ class Profile(models.Model):
   def __str__(self):
     return f'{self.user.username} Profile'
 
-  def __str__(self):
+  def save(self):
     super().save() # override save method of a model
 
     img = Image.open(self.image.path)
+
     if img.height > 300 or img.width > 300:
       output_size = (300, 300)
       img.thumbnail(output_size)
