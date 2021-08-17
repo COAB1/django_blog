85110cd452ae124377f089f792cad94710ee6251
COAB1 <carol.o.ag@gmail.com>
2021-08-17 18:08:28 -0300

    Include LoginRequiredMixin to avoid creating new post without logged in

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import (
     DetailView,
     CreateView,
 )
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
@@ -25,7 +26,7 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
