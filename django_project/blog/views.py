570fffe79b4ccd1a8d59935d953693f9bec78190
COAB1 <carol.o.ag@gmail.com>
2021-08-17 18:02:41 -0300

    Create post-create class based view and template

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,22 +1,39 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+)
 
 
-def home (request):
+def home(request):
     context = {
-        'posts': Post.objects.all() 
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
+
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
 
 class PostDetailView(DetailView):
     model = Post
 
-def about (request):
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+
+def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
